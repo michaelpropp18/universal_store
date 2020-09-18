660aa9acccc2f72b6d799f4ab32ea21a159daf8f
Mason Murphy <mrmurphy888@aol.com>
2020-09-18 13:21:15 -0400

    Create basic past purchases screen layout

--- lib/view/customer/drawer/purchases_screen.dart
+++ lib/view/customer/drawer/purchases_screen.dart
@@ -3,16 +3,33 @@ import 'package:flutter/material.dart';
 class Purchases extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var purchases = <String>[];
+    purchases.add("Item1");
+    purchases.add("Item2");
+    purchases.add("Item3");
+    purchases.add("Price 1");
+    purchases.add("Price 2");
+    purchases.add("Price 3");
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Text('Purchases'),
-          RaisedButton(
-            child: Text('Back'),
-            onPressed: () => Navigator.pop(context),
-          ),
-        ],
+      appBar: AppBar(
+        brightness: Brightness.light,
+        iconTheme: new IconThemeData(
+          color: Colors.black),
+        backgroundColor: Colors.white,
+        title: Text('My Purchases', style: TextStyle(color: Colors.black)),
       ),
+      body: ListView.builder(
+        itemCount: purchases.length,
+        itemBuilder: (context, i) {
+          if (i.isOdd) {
+            return Divider();
+          }
+          return ListTile(
+          title: Text(purchases[i ~/ 2]),
+            trailing: Text(purchases[i ~/ 2 + purchases.length ~/ 2])
+          );
+        }
+      )
     );
   }
 }
