3c14e608817e41e5ec4a7519a35493dcc9da7f61
Michael Propp <michaelpropp8664@gmail.com>
2020-09-20 20:43:20 -0400

    Manager View Inventory List UI

---
+++ lib/view/manager/widgets/store_item.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class StoreItem extends StatelessWidget {
+  final String name;
+  final double price;
+  final int quantity;
+  final String route;
+
+  const StoreItem(
+      {@required this.price, @required this.name, this.route, this.quantity});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width - 75, // TODO fix this
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  name,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold),
+                ),
+                SelectableText(
+                  '\$' + price.toStringAsFixed(2) + ' (x' + quantity.toString() + ')',
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          GestureDetector(
+            child: Icon(Icons.chevron_right),
+            onTap: () {
+              print('got here');
+              //Navigator.pushNamed(context, route);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
