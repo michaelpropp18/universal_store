448763f2f872f33c4f9f90875dd4f820c2a9bc71
Mason Murphy <mrmurphy888@aol.com>
2020-09-21 21:42:41 -0400

    Refactor data models to follow backend model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,4 +1,17 @@
+import 'package:universal_store/models/shopping_trip.dart';
+
 class User {
   final String uid;
+
   User({this.uid});
+
+  List<ShoppingTrip> getShoppingTrips() {
+    // Return dummy data for now
+    final shoppingTrips = <ShoppingTrip>[];
+    for (int i = 0; i < 10; i++) {
+      shoppingTrips.add(ShoppingTrip('Store $i', i + .99, DateTime.now().millisecondsSinceEpoch - (i * 3600 * 24000)));
+    }
+    return shoppingTrips;
+  }
+
 }
--- lib/view_models/past_purchases.dart
+++ lib/view_models/past_purchases.dart
@@ -1,45 +1,16 @@
 import 'package:flutter/foundation.dart';
+import 'package:universal_store/models/shopping_trip.dart';
+import 'package:universal_store/models/user.dart';
 
 class PastPurchasesViewModel with ChangeNotifier {
 
-  String _customerId;
-  final _purchases = <Purchase>[];
+  List<ShoppingTrip> _purchases = <ShoppingTrip>[];
 
   PastPurchasesViewModel() {
-    // Dummy data for now
-    _customerId = "Test Customer";
-    for (int i = 0; i < 10; i++) {
-      purchases.add(Purchase('Store $i', i + .99, DateTime.now().millisecondsSinceEpoch - (i * 3600 * 24000)));
-    }
+    this._purchases = new User().getShoppingTrips();
   }
 
-  String get customerId {
-    return _customerId;
-  }
-
-  List<Purchase> get purchases {
+  List<ShoppingTrip> get purchases {
     return _purchases;
-  }
-
-}
-
-class Purchase {
-
-  String _storeName;
-  double _price;
-  int _date;
-
-  Purchase(this._storeName, this._price, this._date);
-
-  String get storeName {
-    return _storeName;
-  }
-
-  double get price {
-    return _price;
-  }
-
-  DateTime get date {
-    return DateTime.fromMillisecondsSinceEpoch(_date).toLocal();
   }
 }
