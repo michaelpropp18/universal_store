2f7a87dd0f45e21ee81b2d29b73ef60ec7fb1973
Mason Murphy <mrmurphy888@aol.com>
2020-09-25 15:32:25 -0400

    Add FutureBuilder to past_purchases

--- lib/view/customer/drawer/purchases_screen.dart
+++ lib/view/customer/drawer/purchases_screen.dart
@@ -3,37 +3,61 @@ import 'package:flutter/material.dart';
 // import 'package:provider/provider.dart';
 import 'package:universal_store/models/current_user.dart';
 import 'package:universal_store/models/customer.dart';
+import 'package:universal_store/models/order.dart';
 
-class Purchases extends StatelessWidget {
+class Purchases extends StatefulWidget {
+  @override
+  _PurchasesState createState() => _PurchasesState();
+}
+
+class _PurchasesState extends State<Purchases> {
+  Customer user;
+  List<Order> purchases;
+
+  void initState() {
+    super.initState();
+    user = CurrentUser.user;
+  }
+
+  // void getPastOrders() async {
+  //   this.purchases = await user.getOrders();
+  // }
+
   @override
   Widget build(BuildContext context) {
-    // final viewModel = Provider.of<PastPurchasesViewModel>(context);
-    final Customer user = CurrentUser.user;
-    final purchases = user.getOrders();
-    return Scaffold(
-      appBar: AppBar(
-        brightness: Brightness.light,
-        iconTheme: new IconThemeData(
-          color: Colors.black),
-        backgroundColor: Colors.white,
-        title: Text('My Purchases', style: TextStyle(color: Colors.black)),
-      ),
-      body: ListView.builder(
-        itemCount: purchases.length,
-        itemBuilder: (context, i) {
-          if (i.isOdd) {
-            return Divider();
-          }
-          final index = i ~/ 2;
-          final purchase = purchases[index];
-          return ListTile(
-          leading: Icon(Icons.home),
-          title: Text(purchase.storeName),
-          subtitle: Text('\$${purchases[index].price}'),
-          trailing: Icon(Icons.arrow_right)
+    return FutureBuilder(
+      future: user.getOrders(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Text('No Data Yet');
+        } else {
+          return Scaffold(
+            appBar: AppBar(
+              brightness: Brightness.light,
+              iconTheme: new IconThemeData(
+                color: Colors.black),
+              backgroundColor: Colors.white,
+              title: Text('My Purchases', style: TextStyle(color: Colors.black)),
+            ),
+            body: purchases == null ? Text('Nothing yet') : ListView.builder(
+              itemCount: purchases.length,
+              itemBuilder: (context, i) {
+                if (i.isOdd) {
+                  return Divider();
+                }
+                final index = i ~/ 2;
+                final purchase = purchases[index];
+                return ListTile(
+                leading: Icon(Icons.home),
+                title: Text(purchase.store.storeName),
+                subtitle: Text('\$${purchases[index].total}'),
+                trailing: Icon(Icons.arrow_right)
+                );
+              }
+            )
           );
         }
-      )
+      }
     );
   }
 }
