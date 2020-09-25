1d9e14d1ce46542cfa70cca2f2c54ccbdd8e013c
Aaron Anderson <aanderson99@gatech.edu>
2020-09-25 14:06:01 -0400

    getOrders bugfix

--- lib/models/current_user.dart
+++ lib/models/current_user.dart
@@ -3,7 +3,13 @@ import 'package:universal_store/services/auth.dart';
 
 class CurrentUser {
   static User _user;
-  static User get user => _user;
+  static User get user {
+    if (_user == null) {
+      signIn();
+    }
+    return _user;
+  }
+  
   static final AuthService _auth = AuthService();
 
   static signIn() async {
