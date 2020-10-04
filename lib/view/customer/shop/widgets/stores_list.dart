082a15feb9629d511301d704d7e853dec8f5c6ff
Mason Murphy <mrmurphy888@aol.com>
2020-10-04 16:39:31 -0400

    Improve aesthetic of list items

--- lib/view/customer/shop/widgets/stores_list.dart
+++ lib/view/customer/shop/widgets/stores_list.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:universal_store/models/current_user.dart';
 import 'package:universal_store/models/customer.dart';
-import 'package:universal_store/view/customer/home/widgets/store_card.dart';
+import 'package:universal_store/routing/routing_constants.dart';
 
 class StoresList extends StatelessWidget {
   final Customer user = CurrentUser.user;
@@ -13,18 +13,17 @@ class StoresList extends StatelessWidget {
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           return ListView.builder(
-            itemCount: snapshot.data.length * 2 - 1,
+            itemCount: snapshot.data.length,
             itemBuilder: (context, i) {
-              if (i.isOdd) {
-                return Divider();
-              }
-              final index = i ~/ 2;
-              final store = snapshot.data[index];
-              return ListTile(
-              leading: Icon(Icons.home),
-              title: Text(store.storeName),
-              subtitle: Text('${snapshot.data[index].storePhone}'),
-              trailing: Icon(Icons.arrow_right)
+              final store = snapshot.data[i];
+              return Card(
+                child: ListTile(
+                  leading: Icon(Icons.home),
+                  title: Text(store.storeName),
+                  subtitle: Text('${store.storePhone}'),
+                  trailing: Icon(Icons.arrow_right),
+                  onTap: () => Navigator.pushNamed(context, ViewStoreRoute)
+                )
               );
             }
           );
