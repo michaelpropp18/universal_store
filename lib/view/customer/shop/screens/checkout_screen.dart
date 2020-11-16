fb794ef7201f4db28fafb18e51e17d6ff59b5987
Michael Propp <michaelpropp8664@gmail.com>
2020-11-16 15:44:43 -0500

    Checkout Screen

--- lib/view/customer/shop/screens/checkout_screen.dart
+++ lib/view/customer/shop/screens/checkout_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:universal_store/models/cart.dart';
 import 'package:universal_store/view/customer/home/widgets/checkout_bottom.dart';
+import 'package:universal_store/view/customer/home/widgets/checkout_item.dart';
 
 class CheckoutScreen extends StatefulWidget {
   final Cart cart;
@@ -22,8 +23,42 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
         backgroundColor: Colors.white,
         title: Text('Confirm Checkout', style: TextStyle(color: Colors.black)),
       ),
-      body: Container(
-        child: Text(widget.cart.subTotal.toString()),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 2.0),
+              child: Text(widget.cart.store.storeName, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 2.0),
+              child: Text(widget.cart.store.storeAddress, style: TextStyle(fontSize: 14),),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 2.0),
+              child: Text('Phone: ' + widget.cart.store.storePhone, style: TextStyle(fontSize: 14),),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Item', style: TextStyle(fontWeight: FontWeight.bold),),
+                  Text('Price', style: TextStyle(fontWeight: FontWeight.bold),),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: widget.cart.items.length,
+                  itemBuilder: (context, index) {
+                    return CheckoutItem(item: widget.cart.items[index]);
+                  }),
+            ),
+            Text('***This is not a receipt'),
+          ],
+        ),
       ),
       bottomNavigationBar: Container(
           child: CheckoutBottom(
