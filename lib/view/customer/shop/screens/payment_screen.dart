4d63d4992d52a7bffa561764561dab79a3e29b11
Michael Propp <michaelpropp8664@gmail.com>
2020-11-16 17:09:35 -0500

    Payment Screen Part 2

--- lib/view/customer/shop/screens/payment_screen.dart
+++ lib/view/customer/shop/screens/payment_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:universal_store/view/customer/shop/widgets/payment_input.dart';
+import 'package:universal_store/view/shared/error_text.dart';
 
 class PaymentScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        //resizeToAvoidBottomInset: false,
         appBar: AppBar(
           brightness: Brightness.light, // this makes the status bar black
           iconTheme: new IconThemeData(
@@ -13,57 +15,69 @@ class PaymentScreen extends StatelessWidget {
           title: Text('Confirm Payment', style: TextStyle(color: Colors.black)),
         ),
         body: Padding(
-          padding: const EdgeInsets.all(20.0),
+          padding:
+              const EdgeInsets.only(left: 20.0, right: 20, top: 5, bottom: 5),
           child: Container(
-              child: Column(
-            children: [
-              PaymentInput(
-                title: 'Payment Amount',
-                text: '\$12.99',
-              ),
-              PaymentInput(
-                title: 'Name on card',
-                hintText: 'John Doe',
-              ),
-              PaymentInput(
-                  title: 'Card number', hintText: '1234 5678 9000 0000'),
-              Row(
-                children: [
-                  PaymentInput(title: 'Expiry date', hintText: 'MM/YY'),
-                  SizedBox(width: 10),
-                  PaymentInput(title: 'Security Code', hintText: '123'),
-                ],
-              ),
-              PaymentInput(title: 'ZIP / Postal code', hintText: '12345'),
-              Container(
-                margin: const EdgeInsets.all(10),
-                child: SizedBox(
-                  height: 50,
-                  width: double.infinity,
-                  child: FlatButton(
-                      child: Text(
-                        'Confirm Payment',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16.0,
-                        ),
-                      ),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12.0),
-                          side: BorderSide(color: Colors.white70)),
-                      color: Colors.blue,
-                      disabledColor: Colors.grey,
-                      onPressed: () {}),
+              child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  'assets/credit_cards.png',
+                  height: 60,
                 ),
-              ),
-              GestureDetector(
-                onTap: () async {
-                  //await CurrentUser.user.deleteCart(cart);
-                  Navigator.pop(context);
-                },
-                child: Text('Return to Checkout'),
-              ),
-            ],
+                PaymentInput(
+                  title: 'Payment Amount',
+                  text: '\$12.99',
+                ),
+                PaymentInput(
+                  title: 'Name on card',
+                  hintText: 'John Doe',
+                ),
+                PaymentInput(
+                    title: 'Card number', hintText: '1234 5678 9000 0000'),
+                Row(
+                  children: [
+                    PaymentInput(title: 'Expiry date', hintText: 'MM/YY'),
+                    SizedBox(width: 10),
+                    PaymentInput(title: 'Security Code', hintText: '123'),
+                  ],
+                ),
+                PaymentInput(title: 'ZIP / Postal code', hintText: '12345'),
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  child: SizedBox(
+                    height: 50,
+                    width: double.infinity,
+                    child: FlatButton(
+                        child: Text(
+                          'Confirm Payment',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16.0,
+                          ),
+                        ),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                            side: BorderSide(color: Colors.white70)),
+                        color: Colors.blue,
+                        disabledColor: Colors.grey,
+                        onPressed: () {}),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: GestureDetector(
+                    onTap: () async {
+                      //await CurrentUser.user.deleteCart(cart);
+                      Navigator.pop(context);
+                    },
+                    child: Text('Return to Checkout'),
+                  ),
+                ),
+                ErrorText('test'),
+              ],
+            ),
           )),
         ));
   }
